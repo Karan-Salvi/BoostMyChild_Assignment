cd6f9d80ad97ffaa0a1dd78e54fd310fb9f1d520
Karan <karansalviwork@gmail.com>
2025-11-23 22:51:32 +0530

    feat: update project setup and add calendar/add icon components

---
+++ Frontend/src/assets/Add.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Add = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="200"
+      height="200"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+      <path fill="#FFFFFF" d="M13 4v7h7v2h-7v7h-2v-7H4v-2h7V4h2Z" />
+    </svg>
+  );
+};
+
+export default Add;
